f8c70d55d13a24d199ce6293aa7ed75cf9eaf8f9
OJddJO <95376647+OJddJO@users.noreply.github.com>
2022-09-15 18:19:48 +0200

    a

--- imc.py
+++ imc.py
@@ -3,6 +3,22 @@ def imc(masse:float, taille:float):
     masse en kg, taille en m\n
     Retourne l'imc"""
     imc = masse/taille**2
-    print(f"IMC = {imc}")
+    return imc
 
-imc(49, 1.74)
+def imc_status(masse:float, taille:float):
+    """float * float -> str\n
+    masse en kg, taille en m\n
+    Retourne votre status"""
+    i = imc(masse, taille)
+    if i < 18:
+        return 'Vous êtes en sous poids'
+    elif i < 24.9:
+        return 'Vous êtes normal'
+    elif i < 29.9:
+        return 'Vous êtes en surpoids'
+    elif i < 34.9:
+        return 'Vous êtes obèse'
+    elif i <= 35:
+        return 'Vous êtes en obésité sévère'
+
+print(imc_status(49, 1.74))
