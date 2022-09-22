9c0306889bbe7f08115f19458203390a065ea020
OJd_dJO <jeremyli2906@hotmail.com>
2022-09-22 15:13:59 +0200

    a

--- bissextile.py
+++ bissextile.py
@@ -1,4 +1,43 @@
-def bissextile(n):
-    return n%4==0 and (n//100)%4==0
+def bissextile():
+    try:
+        n = int(date.get())
+        if n%4==0 and (n//100)%4==0:
+            tmp = "Is Bissextile"
+        else:
+            tmp = "Is Not Bissextile"
+        txt.set(tmp)
+    except:
+        txt.set("The value isn't an integer")
 
-print(bissextile(2000))
+
+from tkinter import *
+from keyboard import *
+
+def kInput():
+    if is_pressed('enter'):
+        bissextile()
+
+main = Tk()
+main.resizable(False, False)
+main.title("Bissextile")
+
+date = Entry(main, width=25, justify=CENTER)
+date.grid(row = 1, column = 1)
+
+txt = StringVar()
+screenReturn =  Label(main,width=25, textvariable=txt)
+screenReturn.grid(row= 2, column = 1)
+
+testButton = Button(main, text="Test if bissextile", command=bissextile)
+testButton.grid(row=3, column=1)
+
+def shutdown():
+    global run
+    run = False
+    main.quit()
+main.protocol("WM_DELETE_WINDOW", shutdown)
+
+run=True
+while run:
+    kInput()
+    main.update()
