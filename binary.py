dc41d9fe0e8c148df5b5b80f75632666e89c38c6
OJd_dJO <jeremyli2906@hotmail.com>
2022-09-27 17:01:58 +0200

    a

--- binary.py
+++ binary.py
@@ -1,11 +1,9 @@
-def bin():
-    x = int(input("Decimal: "))
+def bin(x):
     binary = ""
     binRes = 0
     while x!=0:
         binRes = x % 2
         x = x // 2
         binary = str(binRes) + binary
-    print(binary)
 
-bin()
+print(bin(255))
