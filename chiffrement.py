af615696b085e6806aa41b619f6977a4f9b8802c
OJddJO <95376647+OJddJO@users.noreply.github.com>
2022-11-08 19:26:46 +0100

    a

--- chiffrement.py
+++ chiffrement.py
@@ -1,4 +1,4 @@
-def chiffrement(message) :
+def chiffrement(message):
     chiffre = ""
     for c in message:
         x = ord(c) + 3
@@ -20,11 +20,71 @@ def dechiffrement_caesar(text:str, decalage:int):
         decode = decode + chr(x)
     return decode
 
-print(dechiffrement_caesar("OHNLTOXSEXIKHYWBGYHKFTMBJNXEXIENLVETLLXWNFHGWX", 3))
+print(dechiffrement_caesar("OHNLTOXSEXIKHYWBGYHKFTMBJNXEXIENLVETLLXWNFHGWX", 2))
 
 def casse_caesar(text):
     caractere = [chr(x) for x in range(65, 91)]
     text = epure(text)
     for i in range(26):
+        decode = ""
         for c in text:
-            x = caractere.find(c) - i
+            x = caractere.index(c) - i
+            decode = decode + caractere[x]
+        print(decode)
+
+print(casse_caesar("OHNLTOXSEXIKHYWBGYHKFTMBJNXEXIENLVETLLXWNFHGWX"))
+
+def substitution(text:str, alphabet:str):
+    new_alphabet = [c for c in alphabet]
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alpha = [c for c in alpha]
+    text = text.upper()
+    encode = ""
+    for c in text:
+        i = new_alphabet.index(c)
+        encode = encode + alpha[i]
+    return encode
+
+print(substitution("FGFDQOLSTHKGYRTHIBLOJWTQWLLOOSTLMLBDHQXWJWTETLMSTDTDT", "QZERTYUIOPASDFGHJKLMWXCVBN"))
+
+def stat(txt: str):
+    txt = txt.upper()
+    dictionnary = {}
+    for character in txt:
+        if character.isupper():
+            try:
+                dictionnary[character] = dictionnary[character] + 1
+            except:
+                dictionnary[character] = 1
+    return dictionnary
+
+fichier = open("Document.txt", "r").read()
+print(stat(fichier))
+
+def decalage(text):
+    caractere = [chr(x) for x in range(65, 91)]
+    text = epure(text)
+    listOfDecodedText = []
+    for i in range(26):
+        decode = ""
+        for c in text:
+            x = caractere.index(c) - i
+            decode = decode + caractere[x]
+        listOfDecodedText.append(decode)
+    stats = []
+    for text in listOfDecodedText:
+        stats.append(stat(text))
+    nbOfE = 0
+    mostProbable = None
+    for statistic in stats:
+        print(statistic)
+        try:
+            if nbOfE < statistic['E']:
+                mostProbable = statistic
+                nbOfE = statistic['E']
+        except Exception as e:
+            print(e)
+    index = stats.index(mostProbable)
+    return listOfDecodedText[index]
+
+print(decalage("OHNLTOXSEXIKHYWBGYHKFTMBJNXEXIENLVETLLXWNFHGWX"))
